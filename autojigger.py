2cd693bee5f747070077d5124f4433d7a560beaa
kyeremeMCW <160285759+kyeremeMCW@users.noreply.github.com>
2024-06-21 16:58:10 -0500

    Update autojigger.py
    cleaned up useless code, created function for making jig, added user inputs/defaults

--- autojigger.py
+++ autojigger.py
@@ -1,167 +1,127 @@
 import nibabel as nib
 import numpy as np
-
 import vtk
-
-from solid import scad_render_to_file, cube, translate, color, difference, intersection
+from solid import scad_render_to_file, cube, translate, difference
 from solid.objects import import_stl
+import argparse
 
+def do_the_jig(nifti, output_stl_path):
+    loaded_nifti = nib.load(nifti)
+    slice_thickness = np.round(loaded_nifti.header["pixdim"][3],1)
 
-nifti = '/Volumes/Siren/Prostate_data/1454/MRI/Processed/prostate_mask.nii.gz'
-output_stl_path = '/Volumes/Siren/Prostate_data/1454/MRI/Processed/prostate_mold_vtk_smooth_12_scaled_decimate_93_py.stl'
+    # read the file
+    reader = vtk.vtkNIFTIImageReader()
+    reader.SetFileName(nifti)
+    reader.Update()
 
-loaded_nifti = nib.load(nifti)
+    # apply marching cube surface generation
+    surf = vtk.vtkDiscreteMarchingCubes()
+    surf.SetInputConnection(reader.GetOutputPort())
+    surf.SetValue(0, 1) 
+    surf.Update()
 
-slice_thickness = np.round(loaded_nifti.header["pixdim"][3],1)
+    #smoothing the mesh
+    smoother = vtk.vtkSmoothPolyDataFilter()
+    smoother.SetInputConnection(surf.GetOutputPort())
+    smoother.SetNumberOfIterations(10000) #need test this more
+    smoother.Update()
 
-# can be done in a loop if you have multiple files to be processed, speed is guaranteed if GPU is used:)
-filename_nii =  nifti
-filename = filename_nii.split(".")[0]
+    #decimation
+    decimate = vtk.vtkDecimatePro()
+    decimate.SetInputConnection(smoother.GetOutputPort())
+    decimate.SetTargetReduction(0.93)
+    decimate.Update()
 
-label = 1
+    #scaling the prostate
+    scaling_factors = (1.02, 1.02, 1.02) 
+    set_scaler = vtk.vtkTransform()
+    set_scaler.Scale(scaling_factors)
 
-# read the file
-reader = vtk.vtkNIFTIImageReader()
-reader.SetFileName(filename_nii)
-reader.Update()
+    scaler = vtk.vtkTransformPolyDataFilter()
+    scaler.SetTransform(set_scaler)
+    scaler.SetInputConnection(decimate.GetOutputPort())
+    scaler.Update()
 
-vol = reader.GetOutput()
+    #determinging the boundaries of the prostate
+    finalbounds = scaler.GetOutput().GetBounds()
+    finalybound = finalbounds[3] - finalbounds[2]
 
-# apply marching cube surface generation
-surf = vtk.vtkDiscreteMarchingCubes()
-surf.SetInputConnection(reader.GetOutputPort())
-surf.SetValue(0, int(label)) # use surf.GenerateValues function if more than one contour is available in the file
-surf.Update()
+    # calculate the center of the bounds
+    center_x = (finalbounds[0] + finalbounds[1]) / 2
+    center_y = (finalbounds[2] + finalbounds[3]) / 2
+    center_z = (finalbounds[4] + finalbounds[5]) / 2
 
-#smoothing the mesh
-smoother= vtk.vtkSmoothPolyDataFilter()
-smoother.SetInputConnection(surf.GetOutputPort())
+    # Compute the translation vector to move the center to (0, 0, 0)
+    translation = (-center_x, -center_y, -center_z)
 
-# increase this integer set number of iterations if smoother surface wanted
-smoother.SetNumberOfIterations(10000)
-smoother.Update()
+    # Create a vtkTransform to perform translation
+    translation_transform = vtk.vtkTransform()
+    translation_transform.Translate(translation)
 
-decimate = vtk.vtkDecimatePro()
-decimate.SetInputConnection(smoother.GetOutputPort())
-decimate.SetTargetReduction(0.93)
-decimate.Update()
-decimated_poly_data = decimate.GetOutput()
+    # Apply the translation to the scaled polydata
+    translation_filter = vtk.vtkTransformPolyDataFilter()
+    translation_filter.SetTransform(translation_transform)
+    translation_filter.SetInputConnection(scaler.GetOutputPort())
+    translation_filter.Update()
 
+    # Get the translated polydata
+    translated_poly_data = translation_filter.GetOutput()
 
-scaling_factors = (1.02, 1.02, 1.02) # or (1/scale_x, 1/scale_y, 1/scale_z) but slightly big
+    #finding and setting the boundaries of the slicing jig
+    jig_modifiers = (-5, 5, -6, 40, -((slice_thickness) +3), ((slice_thickness) +3)) #may need to tweak modifiers
 
+    jig_bounds = (
+        finalbounds[0] + jig_modifiers[0],
+        finalbounds[1] + jig_modifiers[1],
+        finalbounds[2] + jig_modifiers[2],
+        finalbounds[3] + jig_modifiers[3],
+        finalbounds[4] + jig_modifiers[4],
+        finalbounds[5] + jig_modifiers[5]
+    )
 
-set_scaler = vtk.vtkTransform()
-set_scaler.Scale(scaling_factors)
+    jigsizex = jig_bounds[1] - jig_bounds[0]
+    jigsizey = jig_bounds[3] - jig_bounds[2]
+    jigsizez = jig_bounds[5] - jig_bounds[4]
 
-scaler = vtk.vtkTransformPolyDataFilter()
-scaler.SetTransform(set_scaler)
-scaler.SetInputConnection(decimate.GetOutputPort())
-scaler.Update()
+    jig_size = (jigsizex, jigsizey, jigsizez)
 
-scaled_poly_data = scaler.GetOutput()
+    # save the output
+    writer = vtk.vtkSTLWriter()
+    writer.SetInputData(translated_poly_data)
+    writer.SetFileTypeToASCII()
+    writer.SetFileName(output_stl_path)
+    writer.Write()
 
-finalbounds = scaler.GetOutput().GetBounds()
-finalxbound = finalbounds[1] - finalbounds[0]
-finalybound = finalbounds[3] - finalbounds[2]
-finalzbound = finalbounds[5] - finalbounds[4]
+    return output_stl_path, jig_size, jigsizex,jigsizey, jigsizez, finalybound,  slice_thickness 
 
-
-# Calculate the center of the bounds
-center_x = (finalbounds[0] + finalbounds[1]) / 2
-center_y = (finalbounds[2] + finalbounds[3]) / 2
-center_z = (finalbounds[4] + finalbounds[5]) / 2
-
-# Compute the translation vector to move the center to (0, 0, 0)
-translation = (-center_x, -center_y, -center_z)
-
-# Create a vtkTransform to perform translation
-translation_transform = vtk.vtkTransform()
-translation_transform.Translate(translation)
-
-# Apply the translation to the scaled polydata
-translation_filter = vtk.vtkTransformPolyDataFilter()
-translation_filter.SetTransform(translation_transform)
-translation_filter.SetInputConnection(scaler.GetOutputPort())
-translation_filter.Update()
-
-# Get the translated polydata
-translated_poly_data = translation_filter.GetOutput()
-
-
-bounding_box_modifiers = (-5, 5, -6, 40, -((slice_thickness) +3), ((slice_thickness) +3))
-
-box_bounds = (
-    finalbounds[0] + bounding_box_modifiers[0],
-    finalbounds[1] + bounding_box_modifiers[1],
-    finalbounds[2] + bounding_box_modifiers[2],
-    finalbounds[3] + bounding_box_modifiers[3],
-    finalbounds[4] + bounding_box_modifiers[4],
-    finalbounds[5] + bounding_box_modifiers[5]
-)
-
-boxsizex = box_bounds[1] - box_bounds[0]
-boxsizey = box_bounds[3] - box_bounds[2]
-boxsizez = box_bounds[5] - box_bounds[4]
-
-box_size = (boxsizex, boxsizey, boxsizez)
-
-# save the output
-writer = vtk.vtkSTLWriter()
-writer.SetInputData(translated_poly_data)
-writer.SetFileTypeToASCII()
-
-# file name need to be changed
-# save as the .stl file, can be changed to other surface mesh file
-writer.SetFileName(output_stl_path)
-writer.Write()
-
-
-def main():
-       
-    #stl proc - color, translate, combine
+def main(nifti, output_stl_path):
+    output_stl_path, jig_size, jigsizex,jigsizey, jigsizez, finalybound,  slice_thickness = do_the_jig(nifti, output_stl_path)
+    
     stl_object = import_stl(output_stl_path)
-
-    color_the_stl = color("red")
-    colored_stl = color_the_stl(stl_object)
-
-    stl_translatey = int((boxsizey - (finalybound))+(finalybound/2)) #some calculated value based off boxsizey 
+    stl_translatey = int((jigsizey - (finalybound))+(finalybound/2)) #some calculated value based off jigsizey 
 
     combined_stl = None
 
-    #13 min for copy all then translate
     for i in range(stl_translatey):
-        translated_stl = translate([0, i * 1, 0])(colored_stl)
+        translated_stl = translate([0, i * 1, 0])(stl_object)
         if combined_stl is None:
             combined_stl = translated_stl
         else:
             combined_stl += translated_stl
 
-###############################################################################################################################################
+    jig_translatey = 15 #maybe jigsizey/2
 
-    # box proc - color, translate
-    box_translatey = 15 #maybe boxsizey/2
+    jig = cube(size=jig_size, center=True)
+    translated_jig = translate([0, jig_translatey, 0])(jig) #putting the jig in the correct position
 
-    bounding_cube = cube(size=box_size, center=True)
-    
-    color_the_box = color("blue", 0.6)
-    colored_box = color_the_box(bounding_cube)
-
-    
-    translated_box = translate([0, box_translatey, 0])(colored_box) #putting the box in the correct position
-
- ###############################################################################################################################################
-    #slicer proc - translate, combine
-    slicingx = boxsizex+10 #~infinite
-    slicingy = boxsizey-6 #-3 and +3 from edges
+    slicingx = jigsizex+10 #~infinite
+    slicingy = jigsizey-6 #-3 and +3 from edges
     slicingz = 1 # z + z_gap = slicethickness
-
-    z_gap = slice_thickness - slicingz #
-    num_slots = int((np.round(boxsizez-6))/2)
-
-    slicer = cube(size=[slicingx, slicingy, slicingz], center=True)  #create slicer
-
-    positioned_slicer = translate([0,box_translatey,-((boxsizez/2)-3),])(slicer) #align slicer with cube
+    z_gap = slice_thickness - slicingz
+    num_slots = int((np.round(jigsizez-6))/2)
+    
+    slicer = cube(size=[slicingx, slicingy, slicingz], center=True) 
+    positioned_slicer = translate([0,jig_translatey,-((jigsizez/2)-3),])(slicer) 
 
     combined_slicer = None
 
@@ -172,13 +132,18 @@ def main():
         else:
             combined_slicer += translated_slice
 
+    final_jig = difference()(translated_jig, combined_slicer)
+    final_jig -= final_jig, combined_stl
 
-###############################################################################################################################################   
-    #doing the jig
-    combined_object = difference()(translated_box, combined_slicer)
-    combined_object -= combined_object, combined_stl
-
-    scad_render_to_file(combined_object, '/Users/fkyereme/Documents/scripts/openscad/output.scad')
+    scad_render_to_file(final_jig, '/Users/fkyereme/Documents/scripts/openscad/output.scad')
 
 if __name__ == '__main__':
-    main()
+    default_nifti = '/Volumes/Siren/Prostate_data/1454/MRI/Processed/prostate_mask.nii.gz'
+    default_output_stl_path = '/Volumes/Siren/Prostate_data/1454/MRI/Processed/autojigger_mold.stl'
+
+    parser = argparse.ArgumentParser(description='Process NIfTI file and output STL.')
+    parser.add_argument('nifti', nargs='?', type=str, default=default_nifti, help='Path to the input NIfTI file')
+    parser.add_argument('output_stl_path', nargs='?', type=str, default=default_output_stl_path, help='Path to the output STL file')
+    args = parser.parse_args()
+
+    main(args.nifti, args.output_stl_path)
